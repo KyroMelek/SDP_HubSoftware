81aba27d0d56de91ecdd1213b3f5256a894160a4
Kyro Melek <kyro1199@gmail.com>
2022-11-08 14:58:00 -0500

    WiFi communication using WiFiUDP Library

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,23 @@
-#include "WiFi.h"
-#include "AsyncUDP.h"
+#include <WiFi.h>
 #include <WiFiUdp.h>
 #include <string>
 
-// const char *ssid = "WhiteSky-Standard";
-// const char *password = "9fg6cj5k";
+// const char * ssid = "WhiteSky-Standard";
+// const char * password = "9fg6cj5k";
 
 const char *ssid = "iPhone 12 Pro";
 const char *password = "SeniorDesignTeam08";
 
 // const char * udpAddress = "10.23.141.78";
-// const char *udpAddress = "172.20.10.11";
+const char *udpAddress = "172.20.10.11";
 const int udpPort = 5555;
 
-WiFiUDP udpSend;
+unsigned int localPort = 1234; // local port to listen on
 
-AsyncUDP udp;
+char packetBuffer[255];              // buffer to hold incoming packet
+char ReplyBuffer[] = "acknowledged"; // a string to send back
+
+WiFiUDP Udp;
 
 void setup()
 {
@@ -30,47 +32,51 @@ void setup()
       delay(1000);
     }
   }
-  if (udp.listen(1234))
-  {
-    Serial.print("UDP Listening on IP: ");
-    Serial.println(WiFi.localIP());
-    udp.onPacket([](AsyncUDPPacket packet)
-                 {
-            Serial.print("UDP Packet Type: ");
-            Serial.print(packet.isBroadcast()?"Broadcast":packet.isMulticast()?"Multicast":"Unicast");
-            Serial.print(", From: ");
-            Serial.print(packet.remoteIP());
-            Serial.print(":");
-            Serial.print(packet.remotePort());
-            Serial.print(", To: ");
-            Serial.print(packet.localIP());
-            Serial.print(":");
-            Serial.print(packet.localPort());
-            Serial.print(", Length: ");
-            Serial.print(packet.length());
-            Serial.print(", Data: ");
-            Serial.write(packet.data(), packet.length());
-            Serial.println();
-
-            char* message = new char[packet.length()+1];
-            message = (char *)packet.data(); 
-            message[packet.length()] = '\0';   
-
-            std::string messageString(message, packet.length()+1);
-            int pin = stoi(messageString);
-            pinMode(pin, OUTPUT);
-            digitalWrite(pin, HIGH);
-            //reply to the client
-            udpSend.beginPacket(packet.remoteIP(),udpPort);
-            udpSend.printf("Hello from ESP32 ");
-            udpSend.endPacket();
-            packet.printf("Got %u bytes of data", packet.length()); });
-  }
+  Serial.print("UDP Listening on IP: ");
+  Serial.println(WiFi.localIP());
+  Udp.begin(localPort);
 }
 
 void loop()
 {
-  delay(1000);
-  // Send broadcast
-  udp.broadcast("Anyone here?");
+  int packetSize = Udp.parsePacket();
+
+  if (packetSize)
+  {
+
+    Serial.println();
+    Serial.print("Received packet of size: ");
+    Serial.print(packetSize);
+    Serial.println();
+    Serial.print("From ");
+    IPAddress remoteIp = Udp.remoteIP();
+    Serial.print(remoteIp);
+    Serial.print(", port ");
+    Serial.print(Udp.remotePort());
+    Serial.println();
+
+    // read the packet into packetBufffer
+    int len = Udp.read(packetBuffer, 255);
+    // if (len > 0) {
+
+    //   packetBuffer[len] = 0;
+
+    // }
+
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+    char *message = new char[packetSize + 1];
+    message = packetBuffer;
+    message[packetSize] = '\0';
+
+    std::string messageString(message, packetSize + 1);
+    int pin = stoi(messageString);
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+
+    // send a reply, to the IP address and port that sent us the packet we received
+    Udp.beginPacket(Udp.remoteIP(), 5555);
+    Udp.printf("acknowledged");
+    Udp.endPacket();
+  }
 }
